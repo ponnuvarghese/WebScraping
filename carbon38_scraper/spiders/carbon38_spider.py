9805dd47e0ad39c8f7423a3b2cd21b9e2a20c74d
ponnuvarghese <ponnuvarghese98@gmail.com>
2024-01-18 21:31:54 +0530

    scrapper content

---
+++ carbon38_scraper/spiders/carbon38_spider.py
@@ -0,0 +1,35 @@
+# Importing necessary libraries
+import scrapy
+
+class Carbon38Spider(scrapy.Spider):
+    name = 'carbon38'
+    start_urls = ['https://www.carbon38.com/shop-all-activewear/tops']
+
+    def parse(self, response):
+        # Extracting product information
+        products = response.css('div.ProductItem')
+
+        for product in products:
+            name = product.css('h2.ProductItem__Title a::text').get() 
+            price = product.css('span.ProductItem__Price.Price::text').get()
+            sizes = product.css('div.ProductItem__SizeVariants a.add-size-to-cart::text').get()
+            image_url = product.css('.ProductItem__Image::attr(src)').get()
+            designer = product.css('.ProductItem__Designer::text').get()
+
+
+
+
+            yield {
+                'name': name.strip() if name else None,
+                'price': price.strip() if price else None,
+                'size':sizes.strip() if sizes else None,
+                'image_url':image_url.strip() if image_url else None,
+                'brand':designer.strip() if designer else None,
+            }
+
+        # Follow pagination links if available
+        next_page = response.css('a.Pagination__NavItem[rel="next"]::attr(href)').get()
+        if next_page:
+            yield scrapy.Request(url=next_page, callback=self.parse)
+
+
