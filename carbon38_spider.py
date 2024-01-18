263650dcc0c319f10a5cd466333fe8736db6f123
ponnuvarghese <38486735+ponnuvarghese@users.noreply.github.com>
2024-01-18 22:16:06 +0530

    crawling with pagination

--- carbon38_spider.py
+++ carbon38_spider.py
@@ -1,9 +1,9 @@
-# Importing necessary libraries
+
 import scrapy
 
 class Carbon38Spider(scrapy.Spider):
     name = 'carbon38'
-    start_urls = ['https://www.carbon38.com/shop-all-activewear/tops']
+    start_urls = ['https://carbon38.com/en-in/collections/tops?filter.p.m.custom.available_or_waitlist=1']
 
     def parse(self, response):
         # Extracting product information
@@ -16,20 +16,15 @@ class Carbon38Spider(scrapy.Spider):
             image_url = product.css('.ProductItem__Image::attr(src)').get()
             designer = product.css('.ProductItem__Designer::text').get()
 
-
-
-
             yield {
                 'name': name.strip() if name else None,
                 'price': price.strip() if price else None,
-                'size':sizes.strip() if sizes else None,
-                'image_url':image_url.strip() if image_url else None,
-                'brand':designer.strip() if designer else None,
+                'size': sizes.strip() if sizes else None,
+                'image_url': image_url.strip() if image_url else None,
+                'brand': designer.strip() if designer else None,
             }
 
-        # Follow pagination links if available
-        next_page = response.css('.next::attr(href)').get()
-        if next_page:
-            yield scrapy.Request(url=next_page, callback=self.parse)
-
-
+        # Follow pagination link to the next page if available
+        next_page_url = response.css('a.Pagination__NavItem[rel="next"]::attr(href)').get()
+        if next_page_url:
+            yield scrapy.Request(url=response.urljoin(next_page_url), callback=self.parse)
